68571fac38cc89059b654971416e82a67296145f
don Pablo <don@obeyi.com>
2021-03-15 22:48:47 +0200

    misc

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
-$to = 'info@designesia.com';  //Recipient's E-mail
+$subject = 'Website Messenger'; // Subject of your email
+$to = 'info@quantum.co.za';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
